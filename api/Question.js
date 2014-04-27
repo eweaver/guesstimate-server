25d7cb9cfb3b3e15973d0d7f5c95a248ebf60df8
Eric Weaver <areimus@gmail.com>
2014-04-27 01:21:19 -0400

    Add single question API endpoint

---
+++ api/Question.js
@@ -0,0 +1,35 @@
+var util = require('util'),
+    ApiClass = require('./ApiClass'),
+    Question = require('../objects/Question');
+
+/**
+ * Handle the registration of a new user.
+ *
+ * @constructor
+ */
+var QuestionList = function() {
+
+    /**
+     * Retrieve a single question
+     *
+     * @param {userObject} userObject
+     * @param {String} categoryId
+     * @param {String} id
+     * @param {Function<error, Object>} callback
+     */
+    this.get = function(userObject, categoryId, id, callback) {
+        var question = new Question();
+
+        question.load(categoryId, id, function(err, question) {
+            if(err !== null) {
+                callback(err);
+                return;
+            }
+
+            callback(null, question);
+        });
+    };
+};
+
+util.inherits(QuestionList, ApiClass);
+module.exports = QuestionList;
